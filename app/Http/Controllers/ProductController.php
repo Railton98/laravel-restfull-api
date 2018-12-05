787bf20f9d3ddb95e23c6d5cdfa4ed25cb17b8e9
Railton98 <railtonleal98@gmail.com>
2018-12-05 01:57:11 -0300

    basic crud completed for the product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,4 +16,21 @@ class ProductController extends Controller
     {
         return Product::create($request->all());
     }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function destroy(Request $request, Product $product)
+    {
+        $product->delete();
+        return $product;
+    }
 }
